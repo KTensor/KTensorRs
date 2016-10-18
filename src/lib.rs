fa0de8c53dab88e5d8114b609b2c1bdcd19fd7ba
Kevin Wang <wangkevin448@gmail.com>
2016-10-17 17:18:45 -0700

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+//! `ktensor` module
+//!
+//! # Examples
+//!
+//! ```
+//! assert_eq!(ktensor::hello_world(), "Hello World!");
+//! ```
+
+/// should return the static string `"Hello World!"`
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!(ktensor::hello_world(), "Hello World!");
+/// ```
+pub fn hello_world() -> &'static str {
+    "Hello World!"
+}
---
+++ tests/hello_world.rs
@@ -0,0 +1,6 @@
+extern crate ktensor;
+
+#[test]
+fn hello_world_integration_test(){
+    assert_eq!(ktensor::hello_world(), "Hello World!");
+}
