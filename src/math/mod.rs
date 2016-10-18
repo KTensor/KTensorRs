48d210ed8d20ff397aee6914249b8547b1a285f1
Kevin Wang <wangkevin448@gmail.com>
2016-10-17 21:25:08 -0700

    implement getting values from vector buffer

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,7 +1,10 @@
+/// A pair of coordinates
+pub struct Vec2(pub usize, pub usize);
+
 /// A structure of values
 pub struct Matrix<T> {
     /// 2 dimensional array of rows and columns
-    dim: [usize; 2],
+    dim: Vec2,
     /// `Vector` of values in the `Matrix`
     buffer: Vec<T>,
 }
@@ -17,12 +20,28 @@ impl <T> Matrix<T> {
     /// # Example
     ///
     /// ```
-    /// let matrix = ktensor::math::Matrix::new([2, 3], vec![0, 1, 2, 3, 4, 5]);
+    /// let matrix = ktensor::math::Matrix::new(ktensor::math::Vec2(2, 3), vec![0, 1, 2, 3, 4, 5]);
     /// ```
-    pub fn new(dimensions: [usize; 2], buffer: Vec<T>) -> Matrix<T> {
+    pub fn new(dimensions: Vec2, buffer: Vec<T>) -> Matrix<T> {
         Matrix {
             dim: dimensions,
             buffer: buffer,
         }
+    }
+
+    /// Returns reference to value at `Vec2`
+    ///
+    /// # Arguments
+    ///
+    /// - `Vec2` - coordinates of the value in the `Matrix`
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let matrix = ktensor::math::Matrix::new(ktensor::math::Vec2(2, 3), vec![0, 1, 2, 3, 4, 5]);
+    /// assert_eq!(*matrix.get(ktensor::math::Vec2(1, 2)), 5);
+    /// ```
+    pub fn get(&self, Vec2(x, y): Vec2) -> &T {
+        &self.buffer[x * self.dim.1 + y]
     }
 }
