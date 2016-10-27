a80de633802b52482321033dd3eeea10beb17009
Kevin Wang <wangkevin448@gmail.com>
2016-10-27 16:36:53 -0700

    restructure training

--- src/node/graph.rs
+++ src/node/graph.rs
@@ -4,5 +4,6 @@ use tensor::{Tensor};
 pub trait Graph<T> {
     fn get_id(&self) -> &'static str;
     fn run(&self, state: &Context<T>, variable: &Context<T>) -> Tensor<T>;
-    fn train(&self, state: &Context<T>, variable: &Context<T>, delta_next: &Tensor<T>);
+    fn train(&self, state: &Context<T>, variable: &Context<T>, history: &mut Context<T>);
+    fn backward_pass(&self, gradient: &Tensor<T>, learning_rate: &f64);
 }
--- src/node/junction.rs
+++ src/node/junction.rs
@@ -5,19 +5,19 @@ use node::{Graph};
 
 pub struct Node<T> {
     id: &'static str,
-    op: Box<Fn(&Tensor<T>, &Tensor<T>) -> Tensor<T>>,
-    a: Box<Fn(&Tensor<T>) -> Tensor<T>>,
-    a_prime: Box<Fn(&Tensor<T>) -> Tensor<T>>,
-    param: (Box<Graph<T>>, Box<Graph<T>>),
+    op: Box<Fn(Vec<Tensor<T>>) -> Tensor<T>>,
+    op_train: Box<Fn(Vec<&Tensor<T>>) -> Tensor<T>>,
+    op_prime: Box<Fn(&Tensor<T>) -> Tensor<T>>,
+    param: Vec<Box<Graph<T>>>,
 }
 
 impl <T> Node<T> {
-    pub fn new(&self, node_id: &'static str, operation: Box<Fn(&Tensor<T>, &Tensor<T>) -> Tensor<T>>, activation_function: Box<Fn(&Tensor<T>) -> Tensor<T>>, activation_prime_function: Box<Fn(&Tensor<T>) -> Tensor<T>>, parameter: (Box<Graph<T>>, Box<Graph<T>>)) -> Node<T> {
+    pub fn new(&self, node_id: &'static str, operation: Box<Fn(Vec<Tensor<T>>) -> Tensor<T>>, operation_train: Box<Fn(Vec<&Tensor<T>>) -> Tensor<T>>, operation_prime: Box<Fn(&Tensor<T>) -> Tensor<T>>, parameter: Vec<Box<Graph<T>>>) -> Node<T> {
         Node {
             id: node_id,
             op: operation,
-            a: activation_function,
-            a_prime: activation_prime_function,
+            op_train: operation_train,
+            op_prime: operation_prime,
             param: parameter,
         }
     }
@@ -29,15 +29,30 @@ impl <T> Graph<T> for Node<T> where T: Copy + Mul<Output=T> + Add<Output=T> {
     }
 
     fn run(&self, state: &Context<T>, variable: &Context<T>) -> Tensor<T> {
-        (self.a)(&((self.op)(&self.param.0.run(state, variable), &self.param.1.run(state, variable))))
+        (self.op)(self.param.iter().map(|node| node.run(state, variable)).collect())
     }
 
-    fn train(&self, state: &Context<T>, variable: &Context<T>, delta_next: &Tensor<T>) {
-        let x = self.param.0.run(state, variable);
-        let mut w = self.param.1.run(state, variable);
-        w.transpose();
-        let z = &((self.op)(&x, &w));
-        let a = (self.a)(&z);
-        let delta = (self.a_prime)(&z).product(&(delta_next * &w));
+    fn train(&self, state: &Context<T>, variable: &Context<T>, history: &mut Context<T>) {
+        let vec: Vec<&Tensor<T>> = self.param.iter().map(|node| {
+            node.train(state, variable, history);
+            match history.get(&**node) {
+                Some(x) => x,
+                None => panic!("Node {} does not exist in history", node.get_id()),
+            }
+        }).collect();
+        // for node in self.param.iter() {
+        //     node.train(state, variable, history);
+        //     vec.push(match history.get(&**node) {
+        //         Some(x) => x,
+        //         None => panic!("Node {} does not exist in history", node.get_id()),
+        //     });
+        // }
+        // history.set(self, (self.op_train)(vec));
+    }
+
+    fn backward_pass(&self, gradient: &Tensor<T>, learning_rate: &f64) {
+        for node in self.param.iter() {
+
+        }
     }
 }
