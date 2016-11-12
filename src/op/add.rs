2587f9d4f9b7b4e0f6d7d8ee156e7c48394e9c19
Kevin Wang <wangkevin448@gmail.com>
2016-11-12 12:08:36 -0800

    todo addition prime

--- src/op/add.rs
+++ src/op/add.rs
@@ -31,7 +31,7 @@ fn operation_prime<T>(gradient: &Tensor<T>, _: Vec<&Tensor<T>>) -> Vec<Tensor<T>
 fn calc_dim(dims: Vec<Vec2>) -> Vec2 {
     let Vec2(x1, y1) = dims[0];
     let Vec2(x2, y2) = dims[1];
-    assert!(x2 == 1 || x1 == x2);
+    assert!(x1 == 0 && x2 == 1 || x1 == x2);
     assert_eq!(y1, y2);
     dims[0]
 }
