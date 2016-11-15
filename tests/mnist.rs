ddd2b4cdb496ee1cd149ecfd913c5a1a4161989a
Kevin Wang <wangkevin448@gmail.com>
2016-11-15 01:27:27 -0800

    added loading of sample data

--- tests/mnist.rs
+++ tests/mnist.rs
@@ -1,9 +1,10 @@
 extern crate ktensor as k;
+use k::{Arc, Vec2, Tensor, Context, Graph, State, Variable};
 use std::path::{Path};
 use std::fs::{File};
-use std::io::{self, BufReader, Read};
+use std::io::{BufReader, Read};
 use std::error::{Error};
-use std::cmp::{min};
+use std::cmp;
 
 fn read_u8(reader: &mut Read) -> u8 {
     use std::mem;
@@ -14,7 +15,7 @@ fn read_u8(reader: &mut Read) -> u8 {
         data
     }) {
         Err(reason) => panic!("failed to read u8: {}", Error::description(&reason)),
-        Ok(file)    => file,
+        Ok(byte)    => byte,
     }
 }
 
@@ -26,27 +27,51 @@ fn read_u32(reader: &mut Read) -> u32 {
         let data: u32 = unsafe { mem::transmute(buf) };
         data
     }) {
-        Err(reason) => panic!("failed to read u8: {}", Error::description(&reason)),
-        Ok(file)    => file,
+        Err(reason) => panic!("failed to read u32: {}", Error::description(&reason)),
+        Ok(byte)    => byte,
     }
 }
 
-fn read_mnist(labels_path: &Path, labels_checknum: u32, data_path: &Path, data_checknum: u32, samples: usize) {
-    let mut labels_file = match File::open(&labels_path) {
+fn read_mnist(labels_path: &Path, labels_checknum: u32, data_path: &Path, data_checknum: u32, batch_size: usize, samples: Option<usize>) {
+    let labels_file = match File::open(&labels_path) {
         Err(reason) => panic!("failed to open {}: {}", labels_path.display(), Error::description(&reason)),
         Ok(file)    => file,
     };
-    let mut data_file = match File::open(&data_path) {
+    let data_file = match File::open(&data_path) {
         Err(reason) => panic!("failed to open {}: {}", data_path.display(), Error::description(&reason)),
         Ok(file)    => file,
     };
-    let mut labels_reader = BufReader::new(labels_file);
-    let mut data_reader = BufReader::new(data_file);
-    let labels_magic = u32::from_be(read_u32(&mut labels_reader));
-    let data_magic = u32::from_be(read_u32(&mut data_reader));
-    println!("{} {}", labels_magic, data_magic);
+    let ref mut labels_reader = BufReader::new(labels_file);
+    let ref mut data_reader = BufReader::new(data_file);
+
+    let labels_magic = u32::from_be(read_u32(labels_reader));
+    let data_magic = u32::from_be(read_u32(data_reader));
+
     assert_eq!(labels_magic, labels_checknum);
     assert_eq!(data_magic, data_checknum);
+
+    let labels_count = u32::from_be(read_u32(labels_reader)) as usize;
+    let data_count = u32::from_be(read_u32(data_reader)) as usize;
+    let sample_count = cmp::min(labels_count, data_count);
+    let sample_count = cmp::min(sample_count, samples.unwrap_or(sample_count));
+
+    let rows = u32::from_be(read_u32(data_reader)) as usize;
+    let columns = u32::from_be(read_u32(data_reader)) as usize;
+
+    let mut sample_vec: Vec<(Vec<u8>, Vec<u8>)> = Vec::with_capacity(sample_count/batch_size);
+    for _ in 0..sample_count/batch_size {
+        let mut sample_data = vec![0u8; batch_size*rows*columns];
+        match data_reader.read_exact(sample_data.as_mut()) {
+            Err(reason) => panic!("failed to read data byte array: {}", Error::description(&reason)),
+            Ok(_)       => (),
+        }
+        let mut sample_labels = vec![0u8; batch_size];
+        match labels_reader.read_exact(sample_labels.as_mut()) {
+            Err(reason) => panic!("failed to read labels byte array: {}", Error::description(&reason)),
+            Ok(_)       => (),
+        }
+        sample_vec.push((sample_data, sample_labels));
+    }
 }
 
 #[test]
@@ -54,5 +79,5 @@ fn read_mnist(labels_path: &Path, labels_checknum: u32, data_path: &Path, data_c
 fn mnist(){
     let train_labels_path = Path::new("data/train-labels-idx1-ubyte");
     let train_data_path = Path::new("data/train-images-idx3-ubyte");
-    read_mnist(&train_labels_path, 2049, &train_data_path, 2051, 1);
+    read_mnist(&train_labels_path, 2049, &train_data_path, 2051, 16, Some(4096));
 }
